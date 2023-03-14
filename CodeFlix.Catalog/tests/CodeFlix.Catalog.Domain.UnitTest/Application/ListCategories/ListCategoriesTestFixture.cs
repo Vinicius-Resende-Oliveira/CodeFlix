4ff197d7a16ccce0d710b009061b3e9b8a0a84a8
Vinícius Oliveira <vinicius_rr1@hotmail.com>
2023-03-13 22:39:19 -0300

    Implementando  ListCategoriesTest e UpdateCategoryValidator

---
+++ CodeFlix.Catalog/tests/CodeFlix.Catalog.Domain.UnitTest/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,66 @@
+﻿using CodeFlix.Catalog.Domain.Entity;
+using CodeFlix.Catalog.Domain.Repository;
+using CodeFlix.Catalog.UnitTest.Commons;
+using Moq;
+using Xunit;
+
+namespace CodeFlix.Catalog.UnitTest.Application.ListCategories
+{
+    [CollectionDefinition(nameof(ListCategoriesTestFixture))]
+    public class ListCategoriesTestFixtureCollection 
+        : ICollectionFixture<ListCategoriesTestFixture>
+    { }
+
+    public class ListCategoriesTestFixture 
+        : BaseFixture
+    {
+        public string GetValidCategoryName()
+        {
+            var categoryName = "";
+            while (categoryName.Length < 3)
+            {
+                categoryName = Faker.Commerce.Categories(1)[0];
+            }
+
+            if (categoryName.Length > 255)
+            {
+                categoryName = categoryName[..255];
+            }
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription = Faker.Commerce.ProductDescription();
+            if (categoryDescription.Length > 10_000)
+            {
+                categoryDescription = categoryDescription[..10_000];
+            }
+            return categoryDescription;
+        }
+
+        public bool getRandomBoolen()
+            => (new Random()).NextDouble() < 0.5;
+
+        public Category GetValidCategory()
+            => new Category(
+                GetValidCategoryName(),
+                GetValidCategoryDescription(),
+                getRandomBoolen()
+                );
+
+        public List<Category> GetValidCategoryList(int length = 10)
+        {
+            var list = new List<Category>();
+            for (int i = 0; i < length; i++)
+            {
+                list.Add(GetValidCategory());
+            }
+
+            return list;
+        }
+
+        public Mock<ICategoryRepository> GetRepositoryMock()
+            => new();
+    }
+}
---
+++ CodeFlix.Catalog/tests/CodeFlix.Catalog.Domain.UnitTest/Application/UpdateCategory/UpdateCategoryInputValidatorTest.cs
@@ -0,0 +1,52 @@
+﻿using CodeFlix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FluentAssertions;
+using Xunit;
+
+namespace CodeFlix.Catalog.UnitTest.Application.UpdateCategory
+{
+    [Collection(nameof(UpdateCategoryTestFixture))]
+    public class UpdateCategoryInputValidatorTest
+    {
+        private readonly UpdateCategoryTestFixture _fixture;
+
+        public UpdateCategoryInputValidatorTest(UpdateCategoryTestFixture fixture)
+            => _fixture = fixture;
+
+        [Fact(DisplayName = "ValidationOK")]
+        [Trait("Application", "UpdateCategoryInputValidator - UseCases")]
+        public void ValidationOK()
+        {
+            var validInput = _fixture.GetInput();
+            var validator = new UpdateCategoryInputValidator();
+
+            var validationResult = validator.Validate(validInput);
+
+            validationResult.Should().NotBeNull();
+            validationResult.IsValid.Should().BeTrue();
+            validationResult.Errors.Should().HaveCount(0);
+        }
+
+        [Fact(DisplayName = nameof(DontValidWhenEmptyGuid))]
+        [Trait("Application", "UpdateCategoryInputValidator - UseCases")]
+        public void DontValidWhenEmptyGuid()
+        {
+            var input = _fixture.GetInput(Guid.Empty);
+            var validator = new UpdateCategoryInputValidator();
+
+            var validateResult = validator.Validate(input);
+
+            validateResult.Should()
+                .NotBeNull();
+            validateResult.IsValid
+                .Should()
+                .BeFalse();
+            validateResult.Errors
+                .Should()
+                .HaveCount(1);
+            validateResult.Errors[0]
+                .ErrorMessage
+                .Should()
+                .Be("'Id' deve ser informado.");
+        }
+    }
+}
